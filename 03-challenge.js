2bbd9473611e997ac2392c927e621cce37ab7a96
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-11 09:13:30 -0300

    feat: challengeThree

---
+++ 03-challenge.js
@@ -0,0 +1,24 @@
+const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+const randomChar = () => char[Math.floor(Math.random() * char.length)];
+
+const generarId = () => {
+  let pattern = '';
+  let i = 0;
+  while (i < 4) {
+    let j = 0;
+    while (j < 4) {
+      pattern += randomChar();
+      j += 1;
+    }
+    if (i < 3) {
+      pattern += '-'
+    }
+    i += 1;
+  }
+  return pattern;
+};
+
+const id = generarId();
+
+console.log(id);
