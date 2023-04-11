dd93f3f7a9aeec1997f19f36e4ca3c377a9d7664
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-04-11 11:22:06 -0300

    refactor: division error now is a return

--- 02-challenge.js
+++ 02-challenge.js
@@ -4,11 +4,11 @@ const addition = (numbers) => numbers.reduce((acc, curr) => acc + curr, 0);
 const subtraction = (numbers) => numbers.reduce((acc, curr) => acc - curr, 0);
 const multiplication = (numbers) => numbers.reduce((acc, curr) => acc * curr, 1);
 const divison = (a, b) => {
-  if(b === 0) {
-    throw new Error('The dividend "b" must not be zero!')
+  if (b === 0) {
+    return'The dividend "b" must not be zero!';
   }
   return a / b;
-} 
+};
 
 const challengeTwo = new Map();
 challengeTwo.set('addition', addition);
@@ -20,4 +20,6 @@ console.log('This is the example array ==>', exArr);
 console.log('This is the addition ==>', challengeTwo.get('addition')(exArr));
 console.log('This is the subtraction ==>', challengeTwo.get('subtraction')(exArr));
 console.log('This is the multiplication ==>', challengeTwo.get('multiplication')(exArr));
-console.log('This is the divison ==>', challengeTwo.get('divison')(4,2));
+console.log('This is the divison ==>', challengeTwo.get('divison')(4, 2));
+console.log('This is the divison when dividend is 0 ==>', challengeTwo.get('divison')(4, 0));
+
